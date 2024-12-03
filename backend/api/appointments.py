ffd5e6136d5d5fbca4aa781eecb32a4701476fe9
doublehomixide <apol94535@gmail.com>
2024-12-03 23:05:25 +0300

    переименовал+сделал роль

--- backend/api/appointments.py
+++ backend/api/appointments.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from typing import Annotated, Literal
 from fastapi import APIRouter, Depends
 from backend.CreateReadUpdateDelete.appointment import AppointmentCRUD
-from backend.CreateReadUpdateDelete.depends import get_appointment_crud
+from backend.CreateReadUpdateDelete.depends import get_appointment_crud, PermissionChecker
 from backend.SchemasNModels.schemas.user_n_appointments_schemas import UserInDB
 from backend.authentication.action.user import get_current_user
 
-router = APIRouter(prefix='/appointment', tags=['Управление записями к врачу'])
+router = APIRouter(prefix='/appointment', tags=['Управление записями'])
 
 
 @router.post('/new_appointment', description='Записаться к врачу')
@@ -14,7 +14,8 @@ async def create_appointment(time: datetime,
                              unique_id: str,
                              doctor_name: Literal['testingdoctor', 'gandon'],
                              customer: UserInDB = Depends(get_current_user),
-                             db: AppointmentCRUD = Depends(get_appointment_crud)):
+                             db: AppointmentCRUD = Depends(get_appointment_crud),
+                             is_authorized: bool = Depends(PermissionChecker('doctor'))):
     return await db.create_appointment(time, unique_id, customer, doctor_name)
 
 
