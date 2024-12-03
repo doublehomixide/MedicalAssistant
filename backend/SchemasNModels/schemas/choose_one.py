50e9d9dedd1f4fe6f50eb7a84fdc317426778a3f
doublehomixide <apol94535@gmail.com>
2024-12-03 23:04:40 +0300

    убрал ненужный enum добавил literal

--- backend/SchemasNModels/schemas/choose_one.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Options(str, Enum):
-    username = "username"
-    displayed_name = "displayed_name"
-    email = "email"
-    telephone_number = "telephone_number"
-    password = "password"
--- backend/api/account.py
+++ backend/api/account.py
@@ -1,7 +1,8 @@
+from typing import Literal
+
 from backend.CreateReadUpdateDelete.user import UserCRUD
 from fastapi import APIRouter, Depends
 from backend.CreateReadUpdateDelete.depends import get_user_crud
-from backend.SchemasNModels.schemas.choose_one import Options
 from backend.SchemasNModels.schemas.user_n_appointments_schemas import UserInDB
 from backend.authentication.action.user import get_current_user
 
@@ -14,7 +15,7 @@ async def info_about_me(user: UserInDB = Depends(get_current_user)):
 
 
 @router.put('/update/{type_of_data}', description='Обновить данные учетной записи')
-async def update_user(type_of_data: Options, data: str,
+async def update_user(type_of_data: Literal['username','displayed_name','email','telephone_number','password'], data: str,
                       user: UserInDB = Depends(get_current_user),
                       db: UserCRUD = Depends(get_user_crud)) -> dict:
     return await db.update_current_user_data(type_of_data=type_of_data, data=data, user=user)
